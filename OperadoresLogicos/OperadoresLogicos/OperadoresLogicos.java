d164fbde31096300a509fbd4f6ef0b369c69411d
Leonardo Maranhão <leonardo.maranhao@protonmail.com>
2022-01-03 10:59:07 -0300

    exemplos aula de operadores lógicos

--- OperadoresLogicos/OperadoresLogicos/OperadoresLogicos.java
+++ OperadoresLogicos/OperadoresLogicos/OperadoresLogicos.java
@@ -2,6 +2,16 @@ package OperadoresLogicos;
 
 public class OperadoresLogicos {
 
+    public static void main(String[] args) {
+        /**Tipos:
+         * Conjunção:
+         * Disjunção:
+         * Disjunção exclusiva:
+         * Negação:
+         */
+
+
+    }
     
     
 }
