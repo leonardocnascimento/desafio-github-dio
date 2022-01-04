8a34dd3baf6006d447549854205113efd299ffb5
Leonardo Maranhão <leonardo.maranhao@protonmail.com>
2022-01-04 17:54:56 -0300

    Update EstruturasDeRepeticao.java

--- EstruturasDeRepeticao/EstruturasDeRepeticao/EstruturasDeRepeticao.java
+++ EstruturasDeRepeticao/EstruturasDeRepeticao/EstruturasDeRepeticao.java
@@ -3,7 +3,11 @@ package EstruturasDeRepeticao;
 public class EstruturasDeRepeticao {
 
     public static void main(String[] args) {
-        
+        /**Tipos:
+         * while: expressao logica -> sequencia de codigo (retorna a expressão logica e verifica se foi true)
+         * do while: sequencia de codigo -> expressao logica (a repeticao so ocorre apos a expressao logica for true, caso contrario, apenas faz a leitura da sequencia de codigo apenas uma vez)
+         * for: iniciacao -> teste de expressao contada -> sequencia de codigo -> atualizacao -> retorna ao teste.
+         */
         
 
     }
